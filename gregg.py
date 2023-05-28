26bba3908fcebd90189645d9c0fdc0494c496d98
Peter <pjhale@uw.edu>
2023-05-27 17:14:24 -0700

    Made some UI tweaks.

--- gregg.py
+++ gregg.py
@@ -7,7 +7,6 @@ import time
 import tkinter as tk
 from tkinter import *
 import torch
-import torch.nn as nn
 import torchvision.transforms as transforms
 from train import SoftmaxModel
 
@@ -24,10 +23,9 @@ LOOP_LENGTH = 3  # minimum unwound length of a loop (in LINE_RESOLUTIONs)
 DATA_PATH = './data/'  # default data directory
 DATA_WIDTH = 128  # width of saved training data
 DATA_HEIGHT = 128  # height of saved training data
+PHONEMES = ['ay', 'd', 'ee', 'f', 'h', 'l', 'm', 'n', 'o', 'r', 's', 't', 'v']  # possible phonemes (in data order)
 
 DEVICE = 'cpu'  # device on which to run the net
-
-PHONEMES = ['ay', 'd', 'ee', 'f', 'h', 'l', 'm', 'n', 'o', 'r', 's', 't', 'v']  # possible phonemes (in data order)
 
 # UI and phoneme recorder for Gregg recognition tool
 class Gregg(object):
@@ -40,12 +38,14 @@ class Gregg(object):
         self.frame.pack(side=tk.TOP, fill='x')
         self.clear_button = Button(self.frame, text='Clear', command=self.clear)
         self.clear_button.pack(side='left')
-        self.interpret_button = Button(self.frame, text='Interpret', command=self.interpret)
-        self.interpret_button.pack(side='left')
+        self.read_button = Button(self.frame, text='Read', command=self.read)
+        self.read_button.pack(side='left')
         self.speak_button = Button(self.frame, text='Speak', command=self.speak)
         self.speak_button.pack(side='left')
-        self.train_button = Button(self.frame, text='Train', command=self.label_word)
-        self.train_button.pack(side='left')
+        self.label_button = Button(self.frame, text='Label', command=self.label_word)
+        self.label_button.pack(side='left')
+        self.phoneme_label = Label(self.frame, text='No phonemes.')
+        self.phoneme_label.pack(side='right')
 
         # set up canvas
         self.canvas = Canvas(self.window, bg='white', width=WIDTH, height=HEIGHT)
@@ -59,7 +59,7 @@ class Gregg(object):
 
         # bind hotkeys
         self.window.bind('c', self.clear)
-        self.window.bind('t', self.label_word)
+        self.window.bind('l', self.label_word)
 
         # get the net
         self.net = torch.load('./net.pkl')
@@ -68,9 +68,34 @@ class Gregg(object):
         # run UI loop
         self.window.mainloop()
 
+    # subtract two tuples
+    def sub_tuples(self, a, b):
+        return (a[0] - b[0], a[1] - b[1])
+
     # get a phoneme based on its number
     def get_phoneme(self, num):
         return PHONEMES[num]
+    
+    # get the phoneme images for the current word
+    def get_images(self):
+        images = []
+
+        for index in range(len(self.phoneme_list)):
+            # get the position of the phoneme
+            phoneme_x = min(coords[0] for coords in self.phoneme_list[index])
+            phoneme_y = min(coords[1] for coords in self.phoneme_list[index])
+            phoneme_coords = (phoneme_x, phoneme_y)
+
+            # construct an image of the phoneme
+            phoneme_image = PIL.Image.new('RGB', (DATA_WIDTH, DATA_HEIGHT), color='white')
+            phoneme_draw = PIL.ImageDraw.Draw(phoneme_image)
+            first_coords = self.sub_tuples(self.phoneme_list[index][0], phoneme_coords)
+            if len(self.phoneme_list[index]) == 1:
+                phoneme_draw.ellipse([first_coords, (first_coords[0] + LINE_WIDTH, first_coords[1] + LINE_WIDTH)], fill='black', width=LINE_WIDTH)
+            phoneme_draw.line([self.sub_tuples(coords, phoneme_coords) for coords in self.phoneme_list[index]], fill='black', width=LINE_WIDTH, joint='curve')
+            images.append(phoneme_image)
+
+        return images
 
     # clear the canvas and the current word in memory
     def clear(self, event=None):
@@ -90,7 +115,7 @@ class Gregg(object):
         self.current_phoneme
 
     # use a neural net to predict the phonemes
-    def interpret(self):
+    def read(self):
         # get predicitons for phonemes
         images = [transforms.ToTensor()(image).unsqueeze_(0) for image in self.get_images()]
         outputs = [self.net(image) for image in images]
@@ -99,11 +124,14 @@ class Gregg(object):
         # convert predictions to letters
         self.predictions = [prediction[1].item() for prediction in self.predictions]
         self.predictions = [self.get_phoneme(prediction) for prediction in self.predictions]
-        print(self.predictions)
+        if self.predictions:
+            self.phoneme_label.configure(text='Phonemes: '+''.join(self.predictions))
+        else:
+            self.phoneme_label.configure(text='No phonemes.')
 
     # speak phonemes aloud
     def speak(self):
-        self.interpret()
+        self.read()
         try:
             engine = pyttsx3.init()
             engine.say(''.join(self.predictions))
@@ -111,6 +139,61 @@ class Gregg(object):
         except:
             print('Cannot find text to speech engine.')
 
+    # create a popup window to enter training labels
+    def open_label_enter(self, label_num):
+        self.label_window = Toplevel(self.window)
+        directions_label = Label(self.label_window, text=f'Label for phoneme {label_num}:')
+        directions_label.pack(side='top')
+        self.label_entry = Entry(self.label_window)
+        self.label_entry.pack(side='top', fill='x')
+        self.label_entry.focus_set()
+        self.label_entry.bind('<Return>', lambda event, label_window=self.label_window: self.close_label_enter(label_window))
+        enter_button = Button(self.label_window, text='Enter', command=lambda:self.close_label_enter(self.label_window))
+        enter_button.pack(side='bottom')
+
+    # close the label entry popup window
+    def close_label_enter(self, window, event=None):
+        self.label = self.label_entry.get()
+        window.destroy()
+
+    # prompt the user to enter label data for each phoneme in a word
+    def label_word(self, event=None):
+        images = self.get_images()
+        for index in range(len(images)):
+            # prompt the user for a label
+            self.open_label_enter(index + 1)
+            self.window.wait_window(self.label_window)
+
+            # make sure the directory for the label exists
+            if not os.path.exists(DATA_PATH + self.label):
+                os.makedirs(DATA_PATH + self.label)
+
+            # store the image in the  in the directory
+            filename = str(time.time()) + '.jpg'
+            images[index].save(DATA_PATH + self.label + '/' + filename)
+            print(f'saved {filename}')
+
+        self.clear()
+
+    # add the current phoneme to the phoneme list
+    def add_current_phoneme(self, event=None):
+        if self.current_phoneme and (not self.loop_in_phoneme or len(self.current_phoneme) > LOOP_THRESHOLD):
+            self.phoneme_list.append(self.current_phoneme)
+            self.current_phoneme = self.current_phoneme[-1:]
+        self.draw_phonemes()
+
+    # draw the current phonemes in alternating colors
+    def draw_phonemes(self):
+        colors = ['navy', 'blue', 'light blue']
+        color_index = 0
+        for phoneme in self.phoneme_list:
+            color_index = (color_index + 1) % len(colors)
+            for index in range(len(phoneme)):
+                x_prev = phoneme[index - 1][0] if index > 0 else phoneme[index][0]
+                y_prev = phoneme[index - 1][1] if index > 0 else phoneme[index][1]
+                self.canvas.create_line(x_prev, y_prev, phoneme[index][0], phoneme[index][1],
+                        width=LINE_WIDTH, fill=colors[color_index],
+                        capstyle=ROUND, smooth=TRUE, splinesteps=36)
 
     # start a new phoneme when the mouse is pressed
     def mouse_down(self, event):
@@ -127,7 +210,6 @@ class Gregg(object):
                                 capstyle=ROUND, smooth=TRUE, splinesteps=36)
 
     # draw and record the coordinates of mouse movements
-    # TODO: check for new phonemes while moving (i.e. circles, semi-circles, etc.)
     def mouse_move(self, event):
         if abs(event.x - self.x_prev) > LINE_RESOLUTION or abs(event.y - self.y_prev) > LINE_RESOLUTION:
             # create new phoneme on a sharp bend
@@ -167,87 +249,6 @@ class Gregg(object):
                                     capstyle=ROUND, smooth=TRUE, splinesteps=36)
             self.x_prev = event.x
             self.y_prev = event.y
-
-    # add the current phoneme to the phoneme list
-    def add_current_phoneme(self, event=None):
-        if self.current_phoneme and (not self.loop_in_phoneme or len(self.current_phoneme) > LOOP_THRESHOLD):
-            self.phoneme_list.append(self.current_phoneme)
-            self.current_phoneme = self.current_phoneme[-1:]
-        self.draw_phonemes()
-
-    # draw the current phonemes in alternating colors
-    def draw_phonemes(self):
-        colors = ['navy', 'blue', 'light blue']
-        color_index = 0
-        for phoneme in self.phoneme_list:
-            color_index = (color_index + 1) % len(colors)
-            for index in range(len(phoneme)):
-                x_prev = phoneme[index - 1][0] if index > 0 else phoneme[index][0]
-                y_prev = phoneme[index - 1][1] if index > 0 else phoneme[index][1]
-                self.canvas.create_line(x_prev, y_prev, phoneme[index][0], phoneme[index][1],
-                        width=LINE_WIDTH, fill=colors[color_index],
-                        capstyle=ROUND, smooth=TRUE, splinesteps=36)
-                
-    # create a popup window to enter training labels
-    def open_label_enter(self, label_num):
-        self.label_window = Toplevel(self.window)
-        directions_label = Label(self.label_window, text=f'Label for phoneme {label_num}:')
-        directions_label.pack(side='top')
-        self.label_entry = Entry(self.label_window)
-        self.label_entry.pack(side='top', fill='x')
-        self.label_entry.focus_set()
-        self.label_entry.bind('<Return>', lambda event, label_window=self.label_window: self.close_label_enter(label_window))
-        enter_button = Button(self.label_window, text='Enter', command=lambda:self.close_label_enter(self.label_window))
-        enter_button.pack(side='bottom')
-
-    # close the label entry popup window
-    def close_label_enter(self, window, event=None):
-        self.label = self.label_entry.get()
-        window.destroy()
-
-    # subtract two tuples
-    def sub_tuples(self, a, b):
-        return (a[0] - b[0], a[1] - b[1])
-    
-    # get the phoneme images for the current word
-    def get_images(self):
-        images = []
-
-        for index in range(len(self.phoneme_list)):
-            # get the position of the phoneme
-            phoneme_x = min(coords[0] for coords in self.phoneme_list[index])
-            phoneme_y = min(coords[1] for coords in self.phoneme_list[index])
-            phoneme_coords = (phoneme_x, phoneme_y)
-
-            # construct an image of the phoneme
-            phoneme_image = PIL.Image.new('RGB', (DATA_WIDTH, DATA_HEIGHT), color='white')
-            phoneme_draw = PIL.ImageDraw.Draw(phoneme_image)
-            first_coords = self.sub_tuples(self.phoneme_list[index][0], phoneme_coords)
-            if len(self.phoneme_list[index]) == 1:
-                phoneme_draw.ellipse([first_coords, (first_coords[0] + LINE_WIDTH, first_coords[1] + LINE_WIDTH)], fill='black', width=LINE_WIDTH)
-            phoneme_draw.line([self.sub_tuples(coords, phoneme_coords) for coords in self.phoneme_list[index]], fill='black', width=LINE_WIDTH, joint='curve')
-            images.append(phoneme_image)
-
-        return images
-
-    # prompt the user to enter label data for each phoneme in a word
-    def label_word(self, event=None):
-        images = self.get_images()
-        for index in len(images):
-            # prompt the user for a label
-            self.open_label_enter(index + 1)
-            self.window.wait_window(self.label_window)
-
-            # make sure the directory for the label exists
-            if not os.path.exists(DATA_PATH + self.label):
-                os.makedirs(DATA_PATH + self.label)
-
-            # store the image in the  in the directory
-            filename = str(time.time()) + '.jpg'
-            images[index].save(DATA_PATH + self.label + '/' + filename)
-            print(f'saved {filename}')
-
-        self.clear()
 
 # start Gregg recognition tool
 if __name__ == '__main__':
